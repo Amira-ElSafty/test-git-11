b685ee02db60df890d2fc390fad66e26b9dc8891
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2023-09-03 13:05:19 +0300

    changed background in appbar..

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.orange,
+        backgroundColor: Colors.yellow,
         title: Text(
           'Git1',
           style: TextStyle(fontSize: 25),
